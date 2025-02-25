02d4b874acc2dd19915c5f4c500f26512962676d
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2025-02-24 19:54:54 -0500

    Updated Noise Map Functionality
    - Added several variables for greater noise generation control (octaves, seed, lacunarity, persistence, etc)
    - Added seeding, octave calculation for smoother more detailed noise, and normalization calculation to the Create Noise logic.
    - Added the new parameter values for the Generator script.
    - Added OnValidate() logic to the noise map generator for param clamping (I removed most magic numbers to ensure stability).
    - Added default values to each of these.
    - Tested all changes.

--- TerrainProcGen/Assets/Map Scripts/CreateNoise.cs
+++ TerrainProcGen/Assets/Map Scripts/CreateNoise.cs
@@ -2,32 +2,81 @@ using UnityEngine;
 
 public class CreateNoise
 {
-    public static float[,] GenerateANoiseMap(int noiseMapWidth, int noiseMapHeight, float noiseScale)
+    public static float[,] GenerateANoiseMap(int noiseMapWidth, int noiseMapHeight, int seed, float noiseScale, int octaves, float persistence, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[noiseMapWidth, noiseMapHeight];
         
+        System.Random random = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        
+        int randMax = 100000;
+        int randMin = -100000;
+        
+        for (int i = 0; i < octaves; i++)
+        {
+            float octaveOffsetX = random.Next(randMin, randMax)/100000f + offset.x;
+            float octaveOffsetY = random.Next(randMin, randMax)/100000f + offset.y;
+            octaveOffsets[i] = new Vector2(octaveOffsetX, octaveOffsetY);
+        }
         
         //Clamp scale for 0 value (results in Integer Coordinates
-        //(when using Perlin Noise, integer coordinates ALWAYS provide the same value.)
+        //(When using Perlin noise, integer coordinates ALWAYS provide the same value.)
         if (noiseScale <= 0)
         {
             noiseScale = 0.001f;
         }
+        
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+        
+        float halfWidth = noiseMapWidth / 2f;
+        float halfHeight = noiseMapHeight / 2f;
+        
+        
         //Index by Height position(y) first, THEN Width position (x)
         for (int y = 0; y < noiseMapHeight; y++)
         {
             for (int x = 0; x < noiseMapWidth; x++)
             {
-                //set Sample values from width/height values.
-                float sampleX = x / noiseScale;
-                float sampleY = y / noiseScale;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
                 
-                //Generate Perlin Val using Mathf.PerlinNoise(), and assign within the 2D array.
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinValue;
+                for (int i = 0; i < octaves; i++)
+                {
+                    //Set Sample values from width/height values.
+                    float sampleX = (x - halfWidth) / noiseScale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y - halfHeight) / noiseScale * frequency + octaveOffsets[i].y;
+                
+                    //Generate Perlin Val using Mathf.PerlinNoise(), and assign within the 2D array.
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2-1;
+                    noiseHeight += perlinValue * amplitude;
+                    
+                    amplitude *= persistence;  //Range: 0-1 decreasing, w/ each octave.
+                    frequency *= lacunarity;   //Range: 1+, increases w/ each octave.
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
             }
         }
         
+        //Set up noiseMap values after normalizing the initial values.
+        for (int y = 0; y < noiseMapHeight; y++)
+        {
+            for (int x = 0; x < noiseMapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+
         return noiseMap;
     }
 }
--- TerrainProcGen/Assets/Map Scripts/NoiseMapGenerator.cs
+++ TerrainProcGen/Assets/Map Scripts/NoiseMapGenerator.cs
@@ -1,17 +1,67 @@
+using System;
 using UnityEngine;
 
 public class NoiseMapGenerator : MonoBehaviour
 {
-    public int noiseMapWidth;
-    public int noiseMapHeight;
-    public float noiseScale;
-    public bool autoUpdate;
-
+    //Basic Params
+    public int noiseMapWidth = 25;
+    public int noiseMapHeight = 25;
+    public float noiseScale = 0.03f;
+    
+    //Advanced Params
+    public int mapSeed = 1;
+    public int numOctaves = 4;
+    [Range(0,1)] public float noisePersistence = 0.5f;
+    public float lacunarity = 2.0f;
+    public Vector2 offset = new Vector2(0.0f, 0.0f);
+    
+    //Editor+GUI params
+    public bool autoUpdate  = true;
+    
     public void GenerateMap()
     {
-        float[,] noiseMap = CreateNoise.GenerateANoiseMap(noiseMapWidth, noiseMapHeight, noiseScale);
+        float[,] noiseMap = CreateNoise.GenerateANoiseMap(noiseMapWidth, noiseMapHeight,  mapSeed, noiseScale, numOctaves, noisePersistence, lacunarity, offset);
 
         ApplyMap chunk = FindFirstObjectByType<ApplyMap>();
         chunk.DrawNoiseMap(noiseMap);
+    }
+    
+    //Min-Max params
+    private int noiseMapMinAxisSize = 1;
+    private float noiseScaleMin = 0.01f;
+    private int octavesMinimum = 1;
+    private int octavesMaximum = 28;
+    private float lacunarityMinimum = 1f;
+    private void OnValidate()
+    {
+        //Validate Param values, this is an easy approach.
+        if (noiseMapWidth < noiseMapMinAxisSize)
+        {
+            noiseMapWidth = noiseMapMinAxisSize;
+        }
+        if (noiseMapHeight < noiseMapMinAxisSize)
+        {
+            noiseMapHeight = noiseMapMinAxisSize;
+        }
+
+        if (noiseScale <= 0)
+        {
+            noiseScale = noiseScaleMin;
+        }
+
+        if (numOctaves < octavesMinimum)
+        {
+            numOctaves = octavesMinimum;
+        }
+
+        if (numOctaves > octavesMaximum)
+        {
+            numOctaves = octavesMaximum;
+        }
+
+        if (lacunarity < 1)
+        {
+            lacunarity = lacunarityMinimum;
+        }
     }
 }
