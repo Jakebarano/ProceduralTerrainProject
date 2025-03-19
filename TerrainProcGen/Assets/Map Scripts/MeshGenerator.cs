72b4c27e3ccd67110c3d9dd4d42ada7ba2b933c5
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2025-03-19 17:13:42 -0400

    Updates to Several Files
    - Finished work with Colors
    - started on 3D mesh generation.

---
+++ TerrainProcGen/Assets/Map Scripts/MeshGenerator.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class MeshGenerator
+{
+    public static void GenerateTerrainMesh(float[,] heightMap)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+        MeshData 
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                MeshData.vertices[x + y * width] = ; //TODO: Work from this line.
+            }
+        }
+    }
+}
+
+public class MeshData
+{
+    //Variables
+    public Vector3[] vertices;
+    public int[] triangles;
+    int triangleIndex;
+    
+    //Functions
+    public MeshData(int meshWidth, int meshHeight)
+    {
+        vertices = new Vector3[meshWidth * meshHeight];
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+    }
+
+    public void AddTriangle(int a, int b, int c)
+    {
+        triangles[triangleIndex] = a;
+        triangles[triangleIndex+1] = b;
+        triangles[triangleIndex+2] = c;
+        triangleIndex += 3;
+    }
+}
